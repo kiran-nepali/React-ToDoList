52b7ab75042019aed27128a9903633562daad6f3
kiran-nepali <cuziamnepali@hotmail.com>
2020-06-04 20:50:06 +0100

    Updates

---
+++ precache-manifest.2837dd5c5fe46d550744df8db3f3017c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db735be5e9146fded231be56ae295d85",
+    "url": "/React-ToDoList/index.html"
+  },
+  {
+    "revision": "bf6cc3d41ebd89603895",
+    "url": "/React-ToDoList/static/css/main.de14310c.chunk.css"
+  },
+  {
+    "revision": "bb5c9bc06ca5aa693a14",
+    "url": "/React-ToDoList/static/js/2.25402f46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDoList/static/js/2.25402f46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf6cc3d41ebd89603895",
+    "url": "/React-ToDoList/static/js/main.856d4a0e.chunk.js"
+  },
+  {
+    "revision": "8d8ccc61280152814045",
+    "url": "/React-ToDoList/static/js/runtime-main.9580284e.js"
+  }
+]);

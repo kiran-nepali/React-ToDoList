2fac92d7b2861dc92f214a9ddc3d60e705e9fa90
kiran-nepali <cuziamnepali@hotmail.com>
2020-06-04 11:25:41 +0100

    created to do list that allows to add and delete items from the list

---
+++ src/ListItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ListItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function ListItem(props){
+    const items =  props.items;
+    const listItems = items.map(item => {
+        return <div className="list" key = {item.key}> 
+            <p>{item.text} <span>
+                <FontAwesomeIcon className="faicons" icon='trash' onClick={() => props.deleteItem(item.key)}></FontAwesomeIcon>
+            </span></p>
+            
+        </div>
+    })
+    return(
+    <div>{listItems}</div>
+    )
+}
+
+export default ListItem;
